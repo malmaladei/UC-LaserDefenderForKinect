ff3541feba1280c8775bf35e90d9bbae72bc59d7
Philipp Hoffmann <p.hoffmann@ameria.de>
2016-04-21 17:47:51 +0200

    commit that includes changes made to GestureListener

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,44 +2,47 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-    KinectManager kinectManager;
     
+    // these statics are modified by SimpleGestureListener
+    public static int kinectMoveLeft = 0;
+    public static int kinectMoveRight = 0;
+
     public float speedPlayerShip = 5.0f;
     public float paddingPlayerShip = 1f;
 
     float leftLimitX;
     float rightLimitX;
 
-	// Use this for initialization
-	void Start () {
+    public GUIText gestureInfo;
+
+    void Start () {
         Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
         Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
         leftLimitX = leftmost.x + paddingPlayerShip;
         rightLimitX = rightmost.x - paddingPlayerShip;
 	}
 
-	// Update is called once per frame
 	void Update () {
 
+        KinectManager kinectManager = KinectManager.Instance;
+        
         long primaryUserID;
         long primaryUserIndex;
 
-        if (kinectManager.IsUserDetected()) {
+        /*if (kinectManager.IsUserDetected()) {
             primaryUserID = kinectManager.GetPrimaryUserID();
             primaryUserIndex = kinectManager.GetUserIndexById(primaryUserID);
 
-            Debug.Log("primaryuserid:" + primaryUserID + " - primaryuserindex" + primaryUserIndex);
-        }
+            Debug.Log("primaryuserid: " + primaryUserID + " - primaryuserindex: " + primaryUserIndex);
 
-        // bool leaning = gestureListener.GestureInProgress(primaryUserID, primaryUserIndex, KinectGestures.Gestures.LeanLeft, 0.6, KinectInterop.JointType.ShoulderLeft, new Vector3(0, 0, 0));
+        }*/
 
         // TODO hooks this up to Kinect controls
         // TODO use lean gesture to determine direction of playerShip movement
         // TODO use lean angle to change speedPlayerShip dynamically
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow) || kinectMoveLeft == 1) {
             this.transform.position += Vector3.left * speedPlayerShip * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
+        } else if (Input.GetKey(KeyCode.RightArrow) || kinectMoveRight == 1) {
             this.transform.position += Vector3.right * speedPlayerShip * Time.deltaTime;
         }
 
